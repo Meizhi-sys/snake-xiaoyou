ea7ae47ff61af7db84de83556c300174e12eced8
Meizhi-sys <1092510673@qq.com>
2025-08-09 12:38:38 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,6 @@
-const CACHE_NAME = 'snake-pwa-v6-14';
-const CORE_ASSETS = ['./','./index.html','./manifest.json','./icon-512.png','./bgm.js'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(CORE_ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(cached => {
-      if (cached) return cached;
-      return fetch(req).then(resp => {
-        try {
-          if (req.method === 'GET' && resp && resp.status === 200) {
-            const copy = resp.clone();
-            caches.open(CACHE_NAME).then(c => c.put(req, copy));
-          }
-        } catch (err) {}
-        return resp;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
-});
+const CACHE_NAME='snake-pwa-v6-15-min';
+const CORE=['./','./index.html','./bgm.js','./manifest.json','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
