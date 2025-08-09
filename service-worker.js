9be97c794a69f8785a716399832bcf42f58176d3
Meizhi-sys <1092510673@qq.com>
2025-08-09 11:01:34 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'snake-pwa-v6-3';
+const CACHE_NAME = 'snake-pwa-v6-4';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k))))); self.clients.claim(); });
